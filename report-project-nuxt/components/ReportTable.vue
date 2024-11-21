2ae4aeaffce2ca3ad5e77a32d5492dadf906736f
Abdullah Mutlu <abdullah.mutllu@gmail.com>
2024-11-21 11:31:46 +0300

    .

---
+++ report-project-nuxt/components/ReportTable.vue
@@ -0,0 +1,35 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th v-if="selectedColumns.includes('company')" scope="col">Şirket</th>
+        <th v-if="selectedColumns.includes('branch')" scope="col">Şube</th>
+        <th v-if="selectedColumns.includes('department')" scope="col">Departman</th>
+        <th v-if="selectedColumns.includes('person')" scope="col">Kişi</th>
+        <th v-if="selectedColumns.includes('invoice_items')" scope="col">Fatura Kalemi</th>
+        <th v-if="selectedColumns.includes('content')" scope="col">İçerik</th>
+        <th v-if="selectedColumns.includes('details')" scope="col">Detaylar</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="report in reports" :key="report.id">
+        <th scope="row">{{ report.id }}</th>
+        <td v-if="selectedColumns.includes('company')">{{ report.company }}</td>
+        <td v-if="selectedColumns.includes('branch')">{{ report.branch }}</td>
+        <td v-if="selectedColumns.includes('department')">{{ report.department }}</td>
+        <td v-if="selectedColumns.includes('person')">{{ report.person }}</td>
+        <td v-if="selectedColumns.includes('invoice_items')">{{ report.invoice_items }}</td>
+        <td v-if="selectedColumns.includes('content')">{{ report.content }}</td>
+        <td v-if="selectedColumns.includes('details')">{{ report.details }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+defineProps({
+  reports: Array,
+  selectedColumns: Array,
+});
+</script>
--- report-project-nuxt/pages/index.vue
+++ report-project-nuxt/pages/index.vue
@@ -89,34 +89,7 @@
     </div>
 
     <!-- Tablo -->
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th v-if="selectedColumns.includes('company')" scope="col">Şirket</th>
-          <th v-if="selectedColumns.includes('branch')" scope="col">Şube</th>
-          <th v-if="selectedColumns.includes('department')" scope="col">Departman</th>
-          <th v-if="selectedColumns.includes('person')" scope="col">Kişi</th>
-          <th v-if="selectedColumns.includes('invoice_items')" scope="col">Fatura Kalemi</th>
-          <th v-if="selectedColumns.includes('content')" scope="col">İçerik</th>
-          <th v-if="selectedColumns.includes('details')" scope="col">Detaylar</th>
-          <!-- <th v-if="kapat">Boş</th> -->
-        </tr>
-        <!-- <button @click="kapatDegistir">tıkla</button> -->
-      </thead>
-      <tbody>
-        <tr v-for="report in filteredReports" :key="report.id">
-          <th scope="row">{{ report.id }}</th>
-          <td v-if="selectedColumns.includes('company')">{{ report.company }}</td>
-          <td v-if="selectedColumns.includes('branch')">{{ report.branch }}</td>
-          <td v-if="selectedColumns.includes('department')">{{ report.department }}</td>
-          <td v-if="selectedColumns.includes('person')">{{ report.person }}</td>
-          <td v-if="selectedColumns.includes('invoice_items')">{{ report.invoice_items }}</td>
-          <td v-if="selectedColumns.includes('content')">{{ report.content }}</td>
-          <td v-if="selectedColumns.includes('details')">{{ report.details }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <ReportTable :reports="filteredReports" :selectedColumns="selectedColumns" />
   </div>
 </template>
 
@@ -265,26 +238,6 @@ li:hover {
 }
 .font-semibold {
   font-weight: 600;
-}
-.test {
-  min-height: 200px;
-  max-height: 200px;
-  width: 240px;
-  margin-right: 10px;
-}
-.test1 {
-  display: flex;
-}
-.test3 {
-  margin-left: 125px;
-  width: 90px;
-  height: 33px;
-  border-radius: 10px;
-  margin-top: 5px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 0;
 }
 
 .btnCnt {
